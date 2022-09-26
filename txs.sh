5e94a6994c45b6e7fd83b064330728441e1b1876
Dimokus <Dimokus@mail.ru>
2022-09-26 17:32:42 +0300

    Update txs.sh

--- txs.sh
+++ txs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+binary=$1
 SNAP_RPC=`cat /root/bot/RPC.txt`
 echo "$SNAP_RPC"
-txs=`cat /root/bot/tmp/transaction.txt`
+txs=`cat /root/bot/tmp/explorer.txt`
 JSON="/root/bot/tmp/txs.json"
-text="/root/bot/tmp/txs.txt"
+text="/root/bot/tmp/explorer.txt"
 $binary query tx $txs --node "$SNAP_RPC" -o json | jq -r > $JSON
 
 if  grep MsgCreateValidator $JSON
@@ -39,6 +40,37 @@ echo Memo: `cat $JSON | grep memo | uniq | sed 's/            //;s/"memo": "//;s
 echo Height: `cat $JSON | grep -m1 height | awk '{print$2}'|sed 's/"//;s/",//'`>> $text
 echo Time:    `cat $JSON | grep -m2 "time" | uniq | awk '{print$2}' | sed '2!D;s/"//;s/",//'` >> $text
 
+elif grep MsgVote $JSON
+then
+echo Transaction: > $text
+echo  >> $text
+echo Type:    `cat $JSON | jq -r .logs[0].events[1].type | sed "s/p/P/;s/_/ /"` >> $text 
+echo Proposal ID:  `cat $JSON | jq -r .logs[0].events[1].attributes[1].value | sed "s/p/P/;s/_/ /"` >> $text 
+echo  >> $text
+echo Proposal text: >> $text
+echo `$binary query gov proposal 2 -o json | jq -r .content.description` >> $text
+echo  >> $text
+echo Vote:  `cat $JSON | jq  .tx.body.messages[0].option | sed "s/VOTE_OPTION_//"` >> $text 
+
+
+elif grep MsgUnjail $JSON
+then
+echo Transaction: > $text
+echo  >> $text
+echo Type: Unjail >> $text
+echo  >> $text
+if grep failed $JSON
+then
+ echo Status: Error >> $text
+ echo  >> $text
+ echo Message: `cat $JSON | jq .raw_log`
 else
-echo NOT FOUND > $text
+ echo Status: Sucsessfull >> $text
+ echo  >> $text
+ echo Message: `cat $JSON | jq .raw_log`
+
+fi
+else
+echo RAW_LOG: > $text
+echo `cat $JSON | jq .raw_log` >> $text
 fi
