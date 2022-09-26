d7a7b919407b7976ddb7b69a365f546a8d527b23
Dimokus <Dimokus@mail.ru>
2022-09-26 17:33:18 +0300

    Update txs.sh

--- txs.sh
+++ txs.sh
@@ -74,3 +74,37 @@ else
 echo RAW_LOG: > $text
 echo `cat $JSON | jq .raw_log` >> $text
 fi
+root@app-cbcc8446d-gtnrg:~/bot# cat valoper.sh
+#!/bin/bash
+binary=$1
+valoper=`cat /root/bot/tmp/explorer.txt`
+text=/root/bot/tmp/explorer.txt
+SNAP_RPC=`cat /root/bot/RPC.txt`
+$binary query staking validator $valoper -o json > /root/bot/tmp/valoper.json
+echo Validator information: > $text
+echo >> $text
+echo Moniker: `cat /root/bot/tmp/valoper.json | jq -r .description.moniker` >> $text
+if grep BOND_STATUS_BONDED /root/bot/tmp/valoper.json
+then
+status="Active"
+else
+status="InActive"
+fi
+echo Status: $status >> $text
+echo >> $text
+echo Operator: `cat /root/bot/tmp/valoper.json | jq -r .operator_address` >> $text
+echo Tx crearing validator: >> $text
+echo $valoper
+echo $($binary query txs --events="create_validator.validator=$(echo $valoper)" --node $SNAP_RPC -o json | jq .txs[0].txhash -r) >> $text
+echo >> $text
+echo Website: `cat /root/bot/tmp/valoper.json | jq -r .description.website` >> $text
+echo Details: `cat /root/bot/tmp/valoper.json | jq -r .description.details` >> $text
+echo Identity: `cat /root/bot/tmp/valoper.json | jq -r .description.identity`  >> $text
+echo Contact: `cat /root/bot/tmp/valoper.json | jq -r .description.security_contact` >> $text
+echo >> $text
+echo Jail: `cat /root/bot/tmp/valoper.json | jq -r .jailed` >> $text
+echo >> $text
+echo Bonded: `cat /root/bot/tmp/valoper.json | jq -r .tokens` >> $text
+com=`cat /root/bot/tmp/valoper.json | jq -r .commission.commission_rates.rate | awk '{printf("%.2f\n",$1)}'`
+com=$(echo "$com*100" | bc)
+echo Commission: $com% >> $text
