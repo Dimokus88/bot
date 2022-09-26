d08fa561352658c672ed800b9f0d516d334c963a
Dimokus <Dimokus@mail.ru>
2022-09-26 17:19:24 +0300

    Update balance.sh

--- balance.sh
+++ balance.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+binary=$1
 SNAP_RPC=`cat /root/bot/RPC.txt`
-addr=`cat /root/bot/tmp/address.txt`
+addr=`cat /root/bot/tmp/explorer.txt`
+result=/root/bot/tmp/explorer.txt
 $binary query bank balances $addr --node "$SNAP_RPC" -o json > /root/bot/tmp/balance.json
 $binary query staking delegations $addr --node "$SNAP_RPC" -o json > /root/bot/tmp/delegate.json
-echo Balances: > /root/bot//tmp/balance.txt
-echo  >> /root/bot//tmp/balance.txt
-echo Address: $addr >> /root/bot//tmp/balance.txt
+echo Balances: > $result
+echo  >> $result
+echo Address: $addr >> $result
 count=0
 b=`cat /root/bot/tmp/balance.json | jq -r .balances[$count].amount`
 while [[ "$b" -ne "null" ]]
 do
 bal=`cat /root/bot/tmp/balance.json | jq -r .balances[$count].amount`
 den=`cat /root/bot/tmp/balance.json | jq -r .balances[$count].denom`
-echo Spendable Balance: $bal $den >> /root/bot/tmp/balance.txt
+echo Spendable Balance: $bal $den >> $result
 let count=$count+1
 b=`cat /root/bot/tmp/balance.json | jq -r .balances[$count].amount`
 done
+
 count=0
 a=`cat /root/bot/tmp/delegate.json | jq  -r .delegation_responses[$count].balance.amount`
 amount=$a
@@ -24,5 +27,6 @@ do
    let count="$count"+1
    a=`cat /root/bot/tmp/delegate.json | jq  -r .delegation_responses[$count].balance.amount`
    amount=$(echo "$a+$amount" | bc)
+   
 done
-echo Delegated: $amount $den >> /root/bot/tmp/balance.txt
+echo Delegated: $amount $den >> $result
