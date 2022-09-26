687dbc57bff920c04be93ce62d3208d8e30dbf59
Dimokus <Dimokus@mail.ru>
2022-09-26 17:18:36 +0300

    Update CosmoBot.py

--- CosmoBot.py
+++ CosmoBot.py
@@ -3,66 +3,81 @@ from telebot import types
 import os
 import subprocess
 TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot("TOKEN")
 binary = os.getenv("binary")
-bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Welcome to Akash Nodes Alert Bot!")
   markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-  item1=types.KeyboardButton("Info")
-  item2=types.KeyboardButton("Proposal")
-  item3=types.KeyboardButton("About Akash Network")
-  markup.add(item1,item2,item3)
-  bot.send_message(message.chat.id,"Choose a section!",reply_markup=markup)
-
+  item1=types.KeyboardButton("About me")
+  item2=types.KeyboardButton("Main menu")
+  markup.add(item2,item1)
+  message =  bot.send_message(message.chat.id,"Welcome to Akash nodes bot manager!",reply_markup=markup)
+#  bot.delete_message(message.chat.id, message_id=msg.message_id)
 @bot.message_handler(content_types=['text'])
-def handle_text(message):
-  if message.text == "Info":
+def start_message(message):
+  if message.text == "Main menu":
+#     bot.delete_message(message.chat.id, message_id=message.message_id)
+     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+     item1=types.KeyboardButton("Status")
+     item2=types.KeyboardButton("Blockchain explorer")
+     item3=types.KeyboardButton("Manage node")
+     markup.add(item1,item2,item3)
+     bot.send_message(message.chat.id,"You are in the main menu!",reply_markup=markup)
+
+  if message.text == "Status":
+#     bot.delete_message(message.chat.id, message_id=message.message_id)
      markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
      item1=types.KeyboardButton("Status node hardware")
      item2=types.KeyboardButton("Blockchain Parameters")
-     item3=types.KeyboardButton("Request balance")
-     item4=types.KeyboardButton("Request transaction status")
-     item5=types.KeyboardButton("Request block")
-     item6=types.KeyboardButton("Main menu")
-     markup.add(item1,item2,item3)
-     markup.add(item4,item5)
-     markup.add(item6)
-     bot.send_message(message.chat.id,"Info section:",reply_markup=markup)
-  
-  if message.text == "About Akash Network":
+     item3=types.KeyboardButton("Main menu")
+     markup.add(item1,item2)
+     markup.add(item3)
+     bot.send_message(message.chat.id,"Status node & info network:",reply_markup=markup)
+
+  if message.text == "Blockchain explorer":
+#     bot.delete_message(message.chat.id, message_id=message.message_id)
+     a = telebot.types.ReplyKeyboardRemove()
+     text = bot.send_message(message.chat.id,"Enter valoper/address/TX_HASH:",reply_markup=a)
+     bot.register_next_step_handler(text,explorer)
+
+
+  if message.text == "About me":
+#     bot.delete_message(message.chat.id, message_id=message.message_id)
      text = open ('/root/bot/about.txt')
      bot.send_message(message.chat.id,text.read())
 
-  if message.text == "Request balance":
-     text = "Enter address:"
-     a = telebot.types.ReplyKeyboardRemove()
-     text = bot.send_message(message.chat.id,text,reply_markup=a)
-     bot.register_next_step_handler(text,address)
-  if message.text == "Transaction":
-     text = "Enter transaction hash:"
-     text = bot.send_message(message.chat.id,text)
-     bot.register_next_step_handler(text,hash)
-  elif (message.text == "Main menu"):
+  if message.text == "Manage node":
      markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-     item1=types.KeyboardButton("Info")
-     item2=types.KeyboardButton("Proposal")
-     item3=types.KeyboardButton("About Akash Network")
+     item1=types.KeyboardButton("Governance")
+     item2=types.KeyboardButton("Staking")
+     item3=types.KeyboardButton("Send tokens")
+     item4=types.KeyboardButton("Main menu")
      markup.add(item1,item2,item3)
-     bot.send_message(message.chat.id,"Choose a section!",reply_markup=markup)
+     markup.add(item4)
+     bot.send_message(message.chat.id,"Manage a node, vote, or send tokens to a friend!",reply_markup=markup)
+
   if message.text == "Status node hardware":
      subprocess.check_call("/root/bot/status.sh '%s'" % binary, shell=True)
      text = open ('/root/bot/text.txt')
      bot.send_message(message.chat.id,text.read())
-  if message.text == "Request transaction status":
-     text = "Enter TX_HASH:"
-     a = telebot.types.ReplyKeyboardRemove()
-     text = bot.send_message(message.chat.id,text,reply_markup=a)
-     bot.register_next_step_handler(text,hash)
-
+ 
   if message.text == "Blockchain Parameters":
      text = open ('/root/bot/tmp/parameters.txt')
      bot.send_message(message.chat.id,text.read())
+
+
+def explorer(message):
+     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+     item1=types.KeyboardButton("Main menu")
+     item2=types.KeyboardButton("Blockchain explorer")
+     text = message.text
+     file = open('/root/bot/tmp/explorer.txt', 'w')
+     file.write("{explorer}".format(explorer=message.text))
+     file.close()
+     subprocess.check_call("/root/bot/explorer.sh '%s'" % binary , shell=True)
+     text = open ('/root/bot/tmp/explorer.txt')
+     markup.add(item1,item2)
+     bot.send_message(message.chat.id,text.read(),reply_markup=markup)
 
 def hash(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -76,17 +91,5 @@ def hash(message):
     markup.add(item1)
     bot.send_message(message.chat.id,text.read(),reply_markup=markup)
 
-
-def address(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Info")
-    text = message.text
-    file = open('/root/bot/tmp/address.txt', 'w')
-    file.write("{address}".format(address=message.text))
-    file.close()
-    subprocess.check_call("/root/bot/balance.sh '%s'" , shell=True)
-    text = open ('/root/bot/tmp/balance.txt')
-    markup.add(item1)
-    bot.send_message(message.chat.id,text.read(),reply_markup=markup)
-
 bot.infinity_polling()
+
